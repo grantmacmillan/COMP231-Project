06732b2613ef25fa9788ca5ce7e9f3996f813ff7
grant <gmacmil3@my.centennialcollege.ca>
2022-11-21 18:06:28 -0500

    Tool Bar Started

--- app/src/main/java/com/example/comp231_finalproject/MainActivity.java
+++ app/src/main/java/com/example/comp231_finalproject/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.comp231_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView homeIcon = findViewById(R.id.homeIcon);
+        ImageView bellIcon = findViewById(R.id.bellIcon);
+        TextView toolBarTitle = findViewById(R.id.toolBarTitle);
+
+        homeIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Home button clicked", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
