2e2b6e396888191ac514131bed16b327597e5252
grant <gmacmil3@my.centennialcollege.ca>
2022-11-21 19:31:14 -0500

    Popup menu placeholder

--- app/src/main/java/com/example/comp231_finalproject/CalendarActivity.java
+++ app/src/main/java/com/example/comp231_finalproject/CalendarActivity.java
@@ -2,7 +2,12 @@ package com.example.comp231_finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        ImageView homeIcon = findViewById(R.id.homeIcon);
+        ImageView bellIcon = findViewById(R.id.bellIcon);
+        TextView toolBarTitle = findViewById(R.id.toolBarTitle);
+
+        homeIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CalendarActivity.this, MainActivity.class));
+            }
+        });
     }
 }
